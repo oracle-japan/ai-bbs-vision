33de903e90f8e81a7d80057e12b08f52005d825e
shukawam <shukawam@gmail.com>
2024-01-09 17:04:27 +0900

    Delete micronaut application

--- app/src/main/java/me/shukawam/lemon/ResultRepository.java
+++
@@ -1,9 +0,0 @@
-package me.shukawam.lemon;
-
-import io.micronaut.data.annotation.Repository;
-import io.micronaut.data.repository.CrudRepository;
-
-@Repository
-public interface ResultRepository extends CrudRepository<AnalyzeImageResult, String> {
-    
-}
